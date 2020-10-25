381e062d67d90743e86546a7d17e6b3c86379755
d_liyanage <admliyanage@gmail.com>
2020-10-25 18:10:25 +0530

    init

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,6 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Congratulations, You have won! New Game?";
+    public static final String ERROR = "An error has occurred.";
+}
